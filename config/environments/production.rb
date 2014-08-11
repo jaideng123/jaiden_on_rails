fb4683f150a112b93d48dc27d6c8cee961f758b5
jaideng123 <jaideng123@yahoo.com>
2014-08-11 16:17:08 -0500

    Fixed deployment bug again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,10 +5,9 @@ Rails.application.configure do
   config.cache_classes = true
 
   #for deploying css to heroku
-  config.cache_classes = true
   config.serve_static_assets = true
   config.assets.compile = true
-  config.assets.digest = true
+  config.assets.precomplie = true
   
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -25,15 +24,9 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
